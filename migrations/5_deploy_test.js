4e6d528013dd2249eb61e869d6b4bc2f434db689
Aliaksei Hiatsevich <aliaksei.hiatsevich@instinctools.ru>
2018-03-01 13:41:03 +0300

    Fix test contracts deployment step

--- migrations/5_deploy_test.js
+++ migrations/5_deploy_test.js
@@ -2,10 +2,12 @@ var Stub = artifacts.require("./Stub.sol");
 var CAVPlatformTestable = artifacts.require("./CAVPlatformTestable.sol");
 
 module.exports = async (deployer, network) => {
+  if (network === "development") {
     deployer.then(async () => {
         await deployer.deploy(Stub);
         await deployer.deploy(CAVPlatformTestable);
 
         console.log("[MIGRATION] Test deploy: #done")
     })
+  }
 }
